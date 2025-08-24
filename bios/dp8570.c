bc37b9ca6a024550ae2eceb63080871d7ae46c5f
Tom Storey <7754310+tomstorey@users.noreply.github.com>
2025-08-24 14:20:30 +0100

    Tick timer fixed - just needed to specify a clobber

--- bios/dp8570.c
+++ bios/dp8570.c
@@ -73,7 +73,7 @@ interrupt(void)
                 "2:                                         \n\t"
                 :
                 :
-                :
+                :"a0"
             );
         }
     }
